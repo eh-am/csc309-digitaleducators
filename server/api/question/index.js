3c2b98e1b1ef549b8ed4aa1468c1caed8bbf459e
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2015-11-19 01:19:02 -0500

    add a little reminder

--- server/api/question/index.js
+++ server/api/question/index.js
@@ -9,8 +9,10 @@ var router = express.Router();
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', auth.isAuthenticated(), controller.create);
-// router.put('/:id', controller.update);
-// router.patch('/:id', controller.update);
+// TODO
+// uncomment to be able to update questions
+// router.put('/:id', auth.isAuthenticated(), controller.update);
+// router.patch('/:id', auth.isAuthenticated(), controller.update);
 router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
