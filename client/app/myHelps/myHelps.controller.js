adda2e22c8f44378bf6ab4072a9f3e9e18ebe351
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2015-11-23 02:11:32 -0500

    filtering helps

--- client/app/myHelps/myHelps.controller.js
+++ client/app/myHelps/myHelps.controller.js
@@ -5,9 +5,29 @@ angular.module('digitaleducatorsApp')
     $scope.message = 'Hello';
 
     $http.post('/api/questions/myHelps').success(function (helps){
-      $scope.helps = helps;
-      console.log(helps);
-      
+      $scope.myHelps = helps;
+      $scope.helpsStatus = "all";
     });
+
+
+    $scope.filterQuestion = function(question){
+      var filter = false;
+
+      // if the status is "applied", search if the
+      // current user applied to help this person
+      if ($scope.helpsStatus == "applied"){
+        question.applicants.forEach(function (q){
+          if (q.user._id === $scope.$parent.getCurrentUser()._id){
+            filter = true;
+          }
+        });
+      } else {
+        if (question.status === $scope.helpsStatus){
+          filter = true;
+        }
+      }
+
+      return filter;
+    }
 
   });
