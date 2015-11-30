54707cde25918ba282cc545a518b4a742385ac23
Daniela Yassuda <danielayassuda@gmail.com>
2015-11-30 12:17:08 -0500

    Admin page: control buttons, modal before deleting user, hide delete button to prevent deleting oneself

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,17 +1,37 @@
 'use strict';
 
 angular.module('digitaleducatorsApp')
-  .controller('AdminCtrl', function ($scope, $http, Auth, User) {
-
+  .controller('AdminCtrl', function ($scope, $http, Auth, User, $uibModal) {
+    $scope.me = User.get();
+    
     // Use the User $resource to fetch all users
     $scope.users = User.query();
 
     $scope.delete = function(user) {
-      User.remove({ id: user._id });
-      angular.forEach($scope.users, function(u, i) {
-        if (u === user) {
-          $scope.users.splice(i, 1);
-        }
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        size: 'sm'
+      });
+
+      modalInstance.result.then(function (result) {
+        User.remove({ id: user._id });
+        angular.forEach($scope.users, function(u, i) {
+          if (u === user) {
+            $scope.users.splice(i, 1);
+          }
+        });
       });
     };
   });
+
+angular.module('digitaleducatorsApp').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance) {
+  $scope.yes = function () {
+    $uibModalInstance.close(true);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('Cancel');
+  };
+});
