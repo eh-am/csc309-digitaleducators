b0d8a011c65109a98d4f3c05abaa3d8c48e9262f
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2015-12-02 17:00:28 -0500

    fixed a minor mistake (forgot to remove an import of Review on user model)

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -4,7 +4,6 @@ var User = require('./user.model');
 var passport = require('passport');
 var config = require('../../config/environment');
 var jwt = require('jsonwebtoken');
-var Review = require('../review/review.model');
 
 var validationError = function(res, err) {
   return res.status(422).json(err);
