e6eeab2841628e5396f6c0a889b8f35386d6da64
Daniela Yassuda <danielayassuda@gmail.com>
2015-12-03 01:13:15 -0500

    Deleting /api/things requests, review box without collapse, fixing bug on page update after new review is created

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,28 +4,4 @@ angular.module('digitaleducatorsApp')
   .controller('MainCtrl', function ($scope, $http, socket, Auth) {
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.getCurrentUser = Auth.getCurrentUser;
-    
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
-
-    
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
   });
